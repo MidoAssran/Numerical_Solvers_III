2a144a4f40e1919b22ed518bba684d785fc88f20
Mido Assran <midostuf@hotmail.com>
2016-12-01 00:30:39 -0500

    ADDED: problem creation q1

--- experiment.py
+++ experiment.py
@@ -10,41 +10,75 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from functools import reduce
 from utils import *
+
+B = np.array([0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6,
+    1.7, 1.8, 1.9])
+
+H = np.array([0.0, 14.7, 36.5, 71.7, 121.4, 197.4, 256.2, 348.7, 540.6,
+    1062.8, 2318.0, 4781.9, 8687.4, 13924.3, 22650.2])
+
+
+# # F_j is the numerator of the Lagrange Polynomial
+# _p_f = lambda x, r: map(lambda r, x=x: (x-r), filter(lambda r, x=x: x != r, r))
+# F = lambda x, r: reduce(lambda x, y: x* y, _p_f(x,r))
+# lgr_ply = lambda x, i: F(x, i)
+
+def lagrange_polynomial(x, j, dom):
+    """
+    :type x: float
+    :type j: int
+    :type dom: np.array([float])
+    :rtype: float
+    """
+
+    def F(x=x, j=j):
+        val = 1.0
+        for r, x_r in enumerate(dom):
+            if r != j:
+                val *= (x - x_r)
+        return val
+
+    # Return the evaluated lagrange polynomial
+    return F(x, j) / F(dom[j], j)
+
+
+def determine_parameters(dom, target):
+    # One parameter per polynomial
+    # One polynomial per domain point
+    # Each, polynomial is (n-1)th degree
+
+    # Create Matrix
+    G = np.empty([dom.shape[0], dom.shape[0]])
+    G[:] = -1
+    for i, row in enumerate(G):
+        for j, _ in enumerate(row):
+            if G[i,j] == -1:
+                G[i,j] = sum([lagrange_polynomial(x, i, dom) * lagrange_polynomial(x_k, j, dom) for x_k in dom])
+                G[j,i] = G[i,j]
+
+    # Create target
+    b = np.empty([dom.shape[0]])
+    b[:] = -1
+    for i, _ in enumerate(b):
+        b[i] = sum([target(k) * lagrange_polynomial(x_k, i, dom) for k, x_k in enumerate(dom)])
+
+    # Solve LSE
+
+
+def interpolate_first_n(n=6):
+
 
 
 
 if __name__ == "__main__":
 
-    print("\n", end="\n")
-    print("# --------------- TEST --------------- #", end="\n")
-    print("# ------ Choleski Decomposition ------ #", end="\n")
-    print("# --------- (Preconditioned) --------- #", end="\n")
-    print("# ------------------------------------ #", end="\n\n")
-    chol_d = CholeskiDecomposition()
-    # Create a symmetric, real, positive definite matrix.
-    A = matrix_dot_matrix(matrix_transpose(cgfdps._A), cgfdps._A)
-    b = matrix_dot_matrix(matrix_transpose(cgfdps._A), cgfdps._b)
-
-    print("A:\n", A, end="\n\n")
-    print("b:\n", b, end="\n\n")
-    v = chol_d.solve(A=A, b=b)
-    print("result = solve(A, b):\n", v, end="\n\n")
-
-    node_number = cgfdps.map_coordinates_to_node((0.06, 0.04))
-    potential = v[node_number]
-    print("Potential (0.06, 0.04):\n", potential, end="\n\n")
-    print("# ------------------------------------ #", end="\n\n")
-
-
-    # Perform postprocessing of ConjugateGradient residual history
+    # Perform postprocessing
     fig, ax = plt.subplots()
-    norm_2 = [np.linalg.norm(v) for i, v in enumerate(residual_history)]
-    norm_inf = [np.linalg.norm(v, np.inf) for i, v in enumerate(residual_history)]
-    ax.plot(norm_2, 'r', label="2-norm")
-    ax.plot(norm_inf, 'b', label="inf-norm")
+    ax.plot(H, B, 'b', label="Orignal BH")
     legend = ax.legend(loc='best', fontsize='small')
-    plt.title('Residual Norm vs Iteration')
-    plt.ylabel(r'$||b - Ax||$')
-    plt.xlabel('iteration')
+    plt.title('BH Interpolation')
+    plt.ylabel('B')
+    plt.xlabel('H')
     plt.show()
